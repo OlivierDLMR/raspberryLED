d82ce98c08d4543f9c40ed779810e51ee06821cb
OlivierDLMR <odlmr@yahoo.fr>
2020-05-26 14:02:08 +0200

    allumer une LED depuis le navigateur

--- application.py
+++ application.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for
+from led import Led
+import RPi.GPIO as GPIO
 app = Flask(__name__)
+
+#Utilisation d'une norme de nommage pour les broches
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+redLed = Led(14)
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/on')
+def on():
+    redLed.on()
+    return redirect(url_for('home'))
+
+@app.route('/off')
+def off():
+    redLed.off()
+    return redirect(url_for('home'))
+
+
--- led.py
+++ led.py
@@ -2,22 +2,31 @@
 import RPi.GPIO as GPIO
 import time
 
-#Utilisation d'une norme de nommage pour les broches
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+class Led:
+    def __init__(self, numGPIO):
+        # constructeur pour instancier notre objet Led
+        # création d'une variable d'instance "Numéro de GPIO"
+        self.numGPIO = numGPIO
+        # On dit au raspberry qu'on utilise la broche pour "écrire" dessus en mode "sortie"
+        GPIO.setup(self.numGPIO, GPIO.OUT)
+    
+    # méthod "on" pour allumer la led
+    def on(self):
+        print('Led '+str(self.numGPIO)+' on')
+        # On dit à la broche d'envoyer du courant
+        GPIO.output(self.numGPIO, GPIO.HIGH)
 
-#initialisation de la broche en mode "sortie"
-#⚠️ Le nombre passé en paramètre correspond au numéro de GPIO et non au numéro de la broche.
-GPIO.setup(14, GPIO.OUT)
+    # méthod "off" pour éteindre la led
+    def off(self):
+        print('Led '+str(self.numGPIO)+' off')
+        # on dit à la broche d'arrêter d'envoyer du courant
+        GPIO.output(self.numGPIO, GPIO.LOW)
 
-print("Led On")
-#On indique à la pin GPIO 14 que l'on veut envoyer du courant sur celle-ci
-GPIO.output(14, GPIO.HIGH)
-#On demande au script d'attendre 5 secondes
-time.sleep(5)
-#On dit à la pin GPIO 14 d'arrêter d'envoyer du courant.
-GPIO.output(14, GPIO.LOW)
-print("Led Off")
-
-#On indique qu'on a fini d'utiliser les GPIOs
-GPIO.cleanup()
+    def blink(self, numBlink, sleepTime):
+        i = 0
+        while i < numBlink:
+            self.on()
+            time.sleep(sleepTime)
+            self.off()
+            time.sleep(sleepTime)
+            i += 1
