7be26c10f649b759db2a39c4d7bdabebe99220cb
OlivierDLMR <odlmr@yahoo.fr>
2020-05-26 14:38:26 +0200

    allumer Led rouge et bleu

--- application.py
+++ application.py
@@ -8,19 +8,25 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 redLed = Led(14)
+blueLed = Led(15)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/on')
-def on():
-    redLed.on()
+@app.route('/on/<color>')
+def on(color):
+    if color == "red":
+        redLed.on()
+    elif color == "blue":
+        blueLed.on()
     return redirect(url_for('home'))
 
-@app.route('/off')
-def off():
-    redLed.off()
+@app.route('/off/<color>')
+def off(color):
+    if color == "red":
+        redLed.off()
+    elif color == "blue":
+        blueLed.off()
     return redirect(url_for('home'))
-
 
